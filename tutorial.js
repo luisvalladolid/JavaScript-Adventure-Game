d99b7c8ac6a2f792f6b17373f874a7a3462bd01d
luis miguel valladolid <luissvallaa@gmail.com>
2024-12-26 17:15:00 +0800

    first commit

---
+++ tutorial.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")()
+const name = prompt("What is your name? ")
+console.log("Hello", name, "welcome to our game!")
+
+const shouldWePlay = prompt('Do you want to play? ')
+
+if(shouldWePlay.toLowerCase() === "yes") {
+    // Game logic 
+    const leftOrRight = prompt("You enter a maze, do you want to go left or right? ")
+    if(leftOrRight === "left") {
+        console.log("You go left and see a bridge...")
+        const cross = prompt("Do you want to cross the bridge? ").toLowerCase()
+        if (cross === "cross" || cross === "y") {
+            console.log("You cross but the bridge was weak and broke so you fell off. You lost.")
+        } else {
+            console.log("Good choice... You win")
+        }
+    } else {
+        console.log("You go right and fall off a cliff...")
+    }
+
+} else if (shouldWePlay.toLowerCase() === "no") {
+    console.log("Okay :))")
+}
+else {
+    console.log("Invalid input...")
+}
